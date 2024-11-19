d7d3cfceacbcdc4aba1546e3103896231e681c61
5yndr0m <sdilanjana21@gmail.com>
2024-11-19 06:54:44 +0530

    feat: opt into usig components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import React, { useState } from 'react';
 import { students as studentsData } from "./assets/StudentsDb";
+import StuDataTable from "./components/stuDataTable";
+import ProfileView from "./components/profileView";
 
 function App() {
   const [selectedStudent, setSelectedStudent] = useState(studentsData[0]);
@@ -15,8 +17,8 @@ function App() {
   };
 
   return (
-    <div className="main">
-      <h1>Student Information Portal</h1>
+    <div>
+      <h1 className="title">Student Information Portal</h1>
       
       <div className={`app-container ${fontSize}`}>
         
@@ -31,37 +33,10 @@ function App() {
         </div>
   
         <div className="content-container">
-          <div className="students-table">
-            <table>
-              <thead>
-                <tr>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Course</th>
-                  <th>Country</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {studentsData.map((student, index) => (
-                  <tr key={index}>
-                    <td>{student.firstName}</td>
-                    <td>{student.lastName}</td>
-                    <td>{student.course}</td>
-                    <td>{student.country}</td>
-                    <td>
-                      <button onClick={() => handleViewProfile(student)}>
-                        View Profile
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+          <StuDataTable students={studentsData} handleViewProfile={handleViewProfile} />
   
           {/* Right Side - Student Profile */}
-          <div className="student-profile">
+          {/* <div className="student-profile">
             {selectedStudent && (
               <>
                 <img
@@ -76,7 +51,8 @@ function App() {
                 <p><strong>Phone:</strong> {selectedStudent.phone}</p>
               </>
             )}
-          </div>
+          </div> */}
+          <ProfileView selectedStudent={selectedStudent} />
         </div>
       </div>
     </div>
--- src/components/stuDataTable.js
+++ src/components/stuDataTable.js
@@ -1,34 +1,34 @@
-import { Table } from "react-bootstrap";
 
-export default function StuDataTable(props) {
-  
+export default function StuDataTable({ students, handleViewProfile }) {
   
     return (
-        <div className="stuDataTable">
-            <Table className="stuDataTable">
-                <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Course</th>
-                        <th>Country</th>
-                        <th>Profile</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* {props.stuData.map((stu, index) => (
-                        <tr key={index}>
-                            <td>{stu.name}</td>
-                            <td>{stu.email}</td>
-                            <td>{stu.phone}</td>
-                            <td>{stu.address}</td>
-                            <td>{stu.gender}</td>
-                            <td>{stu.birthdate}</td>
-                            <td>{stu.major}</td>
-                        </tr>
-                    ))} */}
-                </tbody>
-            </Table>
-        </div>
+      <div className="students-table">
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Course</th>
+              <th>Country</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {students.map((student, index) => (
+              <tr key={index}>
+                <td>{student.firstName}</td>
+                <td>{student.lastName}</td>
+                <td>{student.course}</td>
+                <td>{student.address.country}</td>
+                <td>
+                  <button onClick={() => handleViewProfile(student)}>
+                    View Profile
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
 }
